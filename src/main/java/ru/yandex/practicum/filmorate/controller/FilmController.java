3be4ceaf38951257e52427e88bf8cafbf1ba3dd7
klen_ars <klenars@yandex.ru>
2022-05-28 10:59:35 +0300

    feat: Добавить эндпоинты и валидацию для Film

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,4 +1,59 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/films")
 public class FilmController {
+    Map<Integer, Film> films = new HashMap<>();
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        int id = film.getId();
+
+        if (films.containsKey(id)){
+            throw new ValidationException("ID already exist!");
+        }
+        if (filmValidation(film)) {
+            films.put(id, film);
+            return film;
+        }
+        return null;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        if (filmValidation(film)) {
+            films.put(film.getId(), film);
+            return film;
+        }
+        return null;
+    }
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    private boolean filmValidation(Film film) {
+        if (film.getName().isBlank()) {
+            throw new ValidationException("Name is empty!");
+        } else if (film.getDescription().length() > 200) {
+            throw new ValidationException("Description is more than 200 characters!");
+        } else if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Release date can't be earlier than 28-12-1895!");
+        } else if (film.getDuration().isNegative()) {
+            throw new ValidationException("Duration can't be negative!");
+        } else {
+            return true;
+        }
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,4 +1,14 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    Map<Integer, User> users = new HashMap<>();
 }
